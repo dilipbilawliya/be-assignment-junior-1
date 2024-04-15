35302fc226f711d711dc21d4a728f1c68fe52a40
Dilip Bilawliya <Dilip92Bilawliya@gmail.com>
2024-04-15 16:48:13 +0530

    Make the partial and added methods in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :friends, foreign_key: 'friend_id'
+
+  def all_friends
+    friend_i_added = Friend.where(user_id: id).pluck(:friend_id)
+    friend_they_added_me = Friend.where(friend_id: id).pluck(:user_id)
+    ids = friend_i_added + friend_they_added_me
+    User.where(id: ids)
+  end
+  
+  def not_friends
+    User.where.not(id: id) - all_friends
+  end
 end
