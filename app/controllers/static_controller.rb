d1b307481a789070a2828b39477bcd711b914da9
Dilip Bilawliya <Dilip92Bilawliya@gmail.com>
2024-04-16 11:39:35 +0530

    Refactor code and some other changes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,5 +7,12 @@ class StaticController < ApplicationController
   end
 
   def person
+    @user = User.find(params[:id])
+    @friends = current_user.all_friends
+    @not_friends = current_user.not_friends
+    @expense_splits = @user.expense_splits
+    unless @user.in?([current_user]+@friends)
+      redirect_to root_path
+    end
   end
 end
